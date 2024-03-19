d21bafbcab1183e340440c0250f7faa728455112
vince19972 <vince19972@gmail.com>
2024-03-19 10:26:21 -0400

    feat: set up favicons and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "../theme/global.css";
 import localFont from "next/font/local";
 
@@ -7,9 +7,67 @@ const pretendard = localFont({
   display: "swap",
 });
 
+const SITE_TITLE = "Staking.xyz";
+const SITE_DESCRIPTION = "Your portal to staking";
+const SITE_URL = "https://staking.xyz";
+const SITE_IMAGE = "/og-image.png";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export const metadata: Metadata = {
-  title: "Staking.xyz",
-  description: "Your portal to staking",
+  metadataBase: new URL(SITE_URL),
+  title: SITE_TITLE,
+  description: SITE_DESCRIPTION,
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        url: "/favicons/favicon.ico",
+        sizes: "32x32",
+      },
+      {
+        rel: "icon",
+        url: "/favicons/favicon.svg",
+        type: "image/svg+xml",
+      },
+    ],
+    apple: [
+      {
+        rel: "apple-touch-icon",
+        url: "/favicons/apple-touch-icon.png",
+        sizes: "180x180",
+      },
+    ],
+  },
+  manifest: "/manifest.webmanifest",
+  openGraph: {
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    type: "website",
+    url: SITE_URL,
+    images: {
+      url: SITE_IMAGE,
+      type: "image/png",
+      width: 1200,
+      height: 630,
+    },
+  },
+  twitter: {
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    creator: "@staking_xyz",
+    card: "summary_large_image",
+    images: {
+      url: SITE_IMAGE,
+      type: "image/png",
+      width: 1200,
+      height: 630,
+    },
+  },
 };
 
 export default function RootLayout({
