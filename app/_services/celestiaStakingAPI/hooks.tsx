3b875d949db6f6fd0b182a1e7d909c748dfbca5b
vince19972 <vince19972@gmail.com>
2024-04-08 17:19:36 -0400

    chore: refetch staking auth check

--- app/_services/celestiaStakingAPI/hooks.tsx
+++ app/_services/celestiaStakingAPI/hooks.tsx
@@ -2,12 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import { getAddressAuthCheck } from ".";
 
 export const useCelestiaAddressAuthCheck = ({ address }: { address?: string }) => {
-  const { data, isLoading, error } = useQuery({
+  const { data, isLoading, error, refetch } = useQuery({
     enabled: !!address,
     queryKey: ["celestiaAddressAuthCheck", address],
     queryFn: () => getAddressAuthCheck(address || ""),
     refetchOnWindowFocus: true,
   });
 
-  return { data, isLoading, error };
+  return { data, isLoading, error, refetch };
 };
