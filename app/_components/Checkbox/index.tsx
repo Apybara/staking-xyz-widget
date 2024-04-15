9986071da152d86905572378effd5eb1a65fa0d1
David Olubaji <lildeyveed@gmail.com>
2024-04-15 20:19:42 +0100

    fix: code changes to maintain consistency and improve a11y

--- app/_components/Checkbox/index.tsx
+++ app/_components/Checkbox/index.tsx
@@ -1,12 +1,12 @@
-import { InputHTMLAttributes, ReactNode } from "react";
+import type { InputHTMLAttributes, ReactNode } from "react";
 import cn from "classnames";
 import { Icon } from "../Icon";
 
 import * as S from "./checkbox.css";
 
-export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+export type CheckboxProps = InputHTMLAttributes<HTMLInputElement> & {
   label: ReactNode;
-}
+};
 
 export const Checkbox = ({ label, checked, ...props }: CheckboxProps) => {
   return (
