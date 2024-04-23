d8513b4b113215067ab652d8c41d8a32fd10873d
David Olubaji <lildeyveed@gmail.com>
2024-04-23 17:24:05 +0100

    refactor rewards hooks and functions to accommodate other networks

--- app/_services/stakingOperator/utils.ts
+++ app/_services/stakingOperator/utils.ts
@@ -1,4 +1,21 @@
+import BigNumber from "bignumber.js";
+import numbro from "numbro";
+
 export const getLastOffset = ({ totalEntries, limit }: { totalEntries: number; limit: number }) => {
   const numberOfPages = Math.ceil(totalEntries / limit);
   return numberOfPages - 1;
+};
+
+export const getCalculatedRewards = (amountStaked: string, rewardRate: number) => {
+  const formattedAmountStaked = BigNumber(amountStaked).toNumber();
+  const base = (formattedAmountStaked * rewardRate) / 100;
+
+  return {
+    percentage: numbro(rewardRate * 100).format({
+      mantissa: 2,
+    }),
+    daily: base / 365,
+    monthly: base / 12,
+    yearly: base,
+  };
 };
