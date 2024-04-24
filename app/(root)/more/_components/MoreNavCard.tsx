3bc1f6233fa8a515be3a4f183158896c37b66223
David Olubaji <lildeyveed@gmail.com>
2024-04-24 23:41:21 +0100

    remove unnecessary code

--- app/(root)/more/_components/MoreNavCard.tsx
+++ app/(root)/more/_components/MoreNavCard.tsx
@@ -5,16 +5,16 @@ import { Icon } from "@/app/_components/Icon";
 import * as S from "./more.css";
 
 export type MoreNavCardProps = {
-  title?: ReactNode;
-  description?: ReactNode;
-  url?: string;
+  title: ReactNode;
+  description: ReactNode;
+  url: string;
 };
 
 export const MoreNavCard = ({ title, description, url }: MoreNavCardProps) => (
   <a href={url} className={S.card} target="_blank" rel="noreferrer">
     <div className={S.main}>
       <span className={S.title}>{title}</span>
-      {description && <span className={S.description}>{description}</span>}
+      <span className={S.description}>{description}</span>
     </div>
     <div className={S.endBox}>
       <span className={cn(S.icon, S.iconChevron)}>
