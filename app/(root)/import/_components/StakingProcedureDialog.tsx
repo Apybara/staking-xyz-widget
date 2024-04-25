bd5724e6fa3a5be0b6d700e47055dc540aa542cf
David Olubaji <lildeyveed@gmail.com>
2024-04-25 17:34:04 +0100

    create import stake button

---
+++ app/(root)/import/_components/StakingProcedureDialog.tsx
@@ -0,0 +1,115 @@
+"use client";
+import type { StakeProcedure, StakeProcedureStep, StakeProcedureState } from "../../../_services/stake/types";
+import { useMemo } from "react";
+import { useRouter } from "next/navigation";
+import { useDialog } from "../../../_contexts/UIContext";
+import { useWallet } from "../../../_contexts/WalletContext";
+import { useStaking } from "../../../_contexts/StakingContext";
+import * as DelegationDialog from "../../../_components/DelegationDialog";
+import { useLinkWithSearchParams } from "../../../_utils/routes";
+
+export const StakingProcedureDialog = () => {
+  const router = useRouter();
+  const { activeWallet } = useWallet();
+  const { procedures, amountInputPad, resetProceduresStates } = useStaking();
+  const { open, toggleOpen } = useDialog("stakingProcedure");
+  const activityLink = useLinkWithSearchParams("activity");
+
+  const uncheckedProcedures = getUncheckedProcedures(procedures || []);
+  const checkedProcedures = getCheckedProcedures(procedures || []);
+  const activeProcedure = getActiveProcedure(procedures || []);
+  const allProceduresCompleted = checkedProcedures.length === (procedures || []).length;
+  const hasLoadingProcedures = getHasLoadingProcedures(procedures || []);
+
+  const ctaText = useMemo(() => {
+    if (!uncheckedProcedures?.[0]) return ctaTextMap.auth.idle;
+    return ctaTextMap[uncheckedProcedures[0].step][uncheckedProcedures[0].state || "idle"];
+  }, [uncheckedProcedures?.[0]?.step, uncheckedProcedures?.[0]?.state]);
+
+  return (
+    <DelegationDialog.Shell
+      dialog={{
+        open: !!open,
+        onOpenChange: () => {
+          if (hasLoadingProcedures) {
+            return;
+          }
+
+          if (open) resetProceduresStates();
+          toggleOpen(!open);
+        },
+      }}
+    >
+      <DelegationDialog.TopBox type="stake" />
+      <DelegationDialog.StepsBox>
+        {procedures?.map((procedure, index) => (
+          <DelegationDialog.StepItem
+            key={index}
+            state={procedure.state || "idle"}
+            onCancel={
+              activeWallet === "okx" || activeWallet?.includes("Mobile")
+                ? () => {
+                    activeProcedure?.setState("active");
+                  }
+                : undefined
+            }
+            tooltip={procedure.tooltip}
+            // explorerUrl={procedure?.txHash && `${networkExplorer[network || "celestia"]}tx/${procedure?.txHash}`}
+          >
+            {procedure.stepName}
+          </DelegationDialog.StepItem>
+        ))}
+      </DelegationDialog.StepsBox>
+      {!allProceduresCompleted ? (
+        <DelegationDialog.CTAButton
+          state={uncheckedProcedures?.[0]?.state === "loading" ? "loading" : "default"}
+          disabled={uncheckedProcedures?.[0]?.state === "loading"}
+          onClick={() => activeProcedure?.send()}
+        >
+          {ctaText}
+        </DelegationDialog.CTAButton>
+      ) : (
+        <DelegationDialog.ResultButtons
+          onActivityButtonClick={() => {
+            router.push(activityLink);
+          }}
+          onDismissButtonClick={() => {
+            amountInputPad.setPrimaryValue("");
+            resetProceduresStates();
+            toggleOpen(false);
+          }}
+        />
+      )}
+    </DelegationDialog.Shell>
+  );
+};
+
+const getUncheckedProcedures = (procedures: Array<StakeProcedure>) => {
+  return procedures.filter((procedure) => procedure.state !== "success");
+};
+const getCheckedProcedures = (procedures: Array<StakeProcedure>) => {
+  return procedures.filter((procedure) => procedure.state === "success");
+};
+const getHasLoadingProcedures = (procedures: Array<StakeProcedure>) => {
+  return procedures.some((procedure) => procedure.state === "loading");
+};
+const getActiveProcedure = (procedures: Array<StakeProcedure>) => {
+  return procedures.find((procedure) => procedure.state !== "success");
+};
+
+const ctaTextMap: Record<StakeProcedureStep, Record<StakeProcedureState, string>> = {
+  auth: {
+    idle: "Approve",
+    active: "Approve",
+    loading: "Approving",
+    success: "Approved",
+    error: "Try again",
+  },
+  delegate: {
+    idle: "Confirm",
+    active: "Confirm",
+    loading: "Confirming",
+    success: "Confirmed",
+    error: "Try again",
+  },
+};
