123796631a710d2a434092dda287cccd1e4c2c49
David Olubaji <lildeyveed@gmail.com>
2024-04-29 14:19:22 +0100

    use skeleton loader for hero cards

--- app/(root)/_components/RewardsNavCard.tsx
+++ app/(root)/_components/RewardsNavCard.tsx
@@ -2,12 +2,14 @@
 import { useNetworkReward } from "@/app/_services/stakingOperator/hooks";
 import { useWallet } from "../../_contexts/WalletContext";
 import * as NavCard from "../_components/NavCard";
+import { Skeleton } from "@/app/_components/Skeleton";
 
 export const RewardsNavCard = (props: NavCard.PageNavCardProps) => {
   const { connectionStatus } = useWallet();
   const networkReward = useNetworkReward();
 
-  const isDisabled = connectionStatus !== "connected";
+  const isLoading = networkReward?.isLoading;
+  const isDisabled = connectionStatus !== "connected" || isLoading;
 
   return (
     <NavCard.Card
@@ -15,8 +17,14 @@ export const RewardsNavCard = (props: NavCard.PageNavCardProps) => {
       page="rewards"
       disabled={isDisabled}
       endBox={{
-        title: <NavCard.SecondaryText>Rewards</NavCard.SecondaryText>,
-        value: <NavCard.PrimaryText>{networkReward?.rewards.percentage} %</NavCard.PrimaryText>,
+        title: (
+          <NavCard.SecondaryText>{isLoading ? <Skeleton width={68} height={12} /> : "Rewards"}</NavCard.SecondaryText>
+        ),
+        value: (
+          <NavCard.PrimaryText>
+            {isLoading ? <Skeleton width={82} height={16} /> : `${networkReward?.rewards.percentage} %`}
+          </NavCard.PrimaryText>
+        ),
       }}
     />
   );
