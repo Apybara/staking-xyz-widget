436b966546c75fd2137030be1de89580c1ae7f5f
David Olubaji <lildeyveed@gmail.com>
2024-04-29 17:44:14 +0100

    hide stake endbox when wallet is disconnected

--- app/(root)/_components/StakeNavCard.tsx
+++ app/(root)/_components/StakeNavCard.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Skeleton } from "@/app/_components/Skeleton";
 import { useShell } from "../../_contexts/ShellContext";
 import { useWallet } from "../../_contexts/WalletContext";
@@ -8,22 +7,32 @@ import { useDynamicAssetValueFromCoin } from "../../_utils/conversions/hooks";
 import * as NavCard from "../_components/NavCard";
 
 export const StakeNavCard = (props: NavCard.PageNavCardProps) => {
-  const { address, activeWallet } = useWallet();
+  const { address, activeWallet, connectionStatus } = useWallet();
   const { network } = useShell();
   const { data: balanceData, isLoading } = useWalletBalance({ address, network, activeWallet }) || {};
   const balance = useDynamicAssetValueFromCoin({ coinVal: balanceData });
+
+  const isDisconnected = connectionStatus !== "connected";
 
   return (
     <NavCard.Card
       {...props}
       page="stake"
       disabled={isLoading}
-      endBox={{
-        title: (
-          <NavCard.SecondaryText>{isLoading ? <Skeleton width={68} height={12} /> : "Available"}</NavCard.SecondaryText>
-        ),
-        value: <NavCard.PrimaryText>{isLoading ? <Skeleton width={82} height={16} /> : balance}</NavCard.PrimaryText>,
-      }}
+      endBox={
+        !isDisconnected
+          ? {
+              title: (
+                <NavCard.SecondaryText>
+                  {isLoading ? <Skeleton width={68} height={12} /> : "Available"}
+                </NavCard.SecondaryText>
+              ),
+              value: (
+                <NavCard.PrimaryText>{isLoading ? <Skeleton width={82} height={16} /> : balance}</NavCard.PrimaryText>
+              ),
+            }
+          : undefined
+      }
     />
   );
 };
