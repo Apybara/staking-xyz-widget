cec5482bc060779ebc0b9714ea835e0fbe0c4bb1
vince19972 <vince19972@gmail.com>
2024-06-27 09:56:59 -0400

    feat: add min stake amount by network

--- app/_contexts/StakingContext/hooks.tsx
+++ app/_contexts/StakingContext/hooks.tsx
@@ -4,22 +4,23 @@ import { useShell } from "../../_contexts/ShellContext";
 import { useWallet } from "../../_contexts/WalletContext";
 import { useWalletBalance } from "../../_services/wallet/hooks";
 import { getBasicAmountValidation, getBasicTxCtaValidation } from "../../_utils/transaction";
-import { defaultNetwork, requiredBalanceStakingByNetwork } from "@/app/consts";
+import { defaultNetwork, requiredBalanceStakingByNetwork, minimumStakingAmountByNetwork } from "@/app/consts";
 
 export const useStakeAmountInputValidation = ({
   inputAmount = "0",
 }: {
   inputAmount: StakingStates["coinAmountInput"];
 }) => {
-  const { network } = useShell();
+  const { network, stakingType } = useShell();
   const { address, activeWallet, connectionStatus } = useWallet();
   const { data: balanceData } = useWalletBalance({ address, network, activeWallet }) || {};
 
   const buffer = useStakeMaxAmountBuffer({ amount: inputAmount });
+  const minAmount = stakingType ? minimumStakingAmountByNetwork[network || defaultNetwork][stakingType] : 0;
 
   const amountValidation = getBasicAmountValidation({
     amount: inputAmount,
-    min: "0",
+    min: (minAmount || 0).toString(),
     max: balanceData,
     bufferValidationAmount: BigNumber(inputAmount).plus(buffer).toString(),
     bufferValidationMax: balanceData,
