9f648e0421ee9c04344ff3bf6c70f156463f6190
David Olubaji <lildeyveed@gmail.com>
2024-06-28 01:33:51 +0100

    add credits for liquid staking

---
+++ app/(root)/_components/LiquidStakingCredits/liquidStakingCredits.css.ts
@@ -0,0 +1,32 @@
+import { style } from "@vanilla-extract/css";
+import { pxToRem } from "../../../../theme/utils";
+import { colors } from "../../../../theme/theme.css";
+
+export const container = style({
+  position: "absolute",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  gap: pxToRem(8),
+  top: "calc(100% + 16px)",
+  left: "0",
+  right: "0",
+});
+
+export const text = style({
+  fontSize: pxToRem(12),
+  color: colors.black600,
+  lineHeight: pxToRem(12),
+});
+
+export const link = style({
+  textDecoration: "underline",
+
+  selectors: {
+    "&:hover": {
+      transition: "color 0.3s",
+      color: colors.black000,
+    },
+  },
+});
--- app/_components/WidgetShell/index.tsx
+++ app/_components/WidgetShell/index.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import type { ReactNode } from "react";
+import { useSearchParams } from "next/navigation";
 import cn from "classnames";
+import { LiquidStakingCredits } from "@/app/(root)/_components/LiquidStakingCredits";
 import { useWidget } from "../../_contexts/WidgetContext";
 import { LoadingSpinner } from "../LoadingSpinner";
 import * as S from "./widgetShell.css";
@@ -13,9 +15,17 @@ export type WidgetShellProps = {
 
 export const WidgetShell = ({ children, className }: WidgetShellProps) => {
   const { status } = useWidget();
+  const searchParams = useSearchParams();
+  const stakingType = searchParams.get("stakingType");
 
   if (status === "loading") {
     return <LoadingSpinner size={24} />;
   }
-  return <section className={cn(S.shell, className)}>{children}</section>;
+
+  return (
+    <>
+      <section className={cn(S.shell, className)}>{children}</section>
+      {stakingType === "liquid" && <LiquidStakingCredits />}
+    </>
+  );
 };
