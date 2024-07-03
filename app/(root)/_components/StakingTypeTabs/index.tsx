eecafdcfe3446da05d9255d814299b61c9e67cf7
David Olubaji <lildeyveed@gmail.com>
2024-07-03 13:05:51 +0100

    disabled liquid staking tab

--- app/(root)/_components/StakingTypeTabs/index.tsx
+++ app/(root)/_components/StakingTypeTabs/index.tsx
@@ -3,9 +3,11 @@ import cn from "classnames";
 import * as S from "./stakingTabs.css";
 import { useStakingTypeChange } from "@/app/_contexts/ShellContext/hooks";
 import { stakingTypeMap } from "@/app/consts";
-import type { StakingType } from "@/app/types";
+import type { StakingTypeTab } from "@/app/types";
+import Tooltip from "@/app/_components/Tooltip";
+import { Icon } from "@/app/_components/Icon";
 
-const tabs = [
+const tabs: Array<StakingTypeTab> = [
   {
     label: stakingTypeMap.native,
     value: "native",
@@ -13,6 +15,7 @@ const tabs = [
   {
     label: stakingTypeMap.liquid,
     value: "liquid",
+    disabled: true,
   },
 ];
 
@@ -21,13 +24,17 @@ export const StakingTypeTabs = () => {
 
   return (
     <ul className={cn(S.tabs)}>
-      {tabs.map(({ label, value }) => (
+      {tabs.map(({ label, value, disabled }) => (
         <li key={`staking-tab-${value}`} className={cn(S.tab)}>
           <button
-            className={cn(S.tabButton({ state: activeStakingType === value ? "highlighted" : "default" }))}
-            onClick={() => onUpdateRouter(value as StakingType)}
+            className={cn(
+              S.tabButton({ state: disabled ? "disabled" : activeStakingType === value ? "highlighted" : "default" }),
+            )}
+            onClick={() => onUpdateRouter(value)}
+            disabled={disabled}
           >
-            {label}
+            <span>{label}</span>
+            {disabled && <Tooltip className={S.tooltip} trigger={<Icon name="info" />} content="Coming soon!" />}
           </button>
         </li>
       ))}
