113a5a72d7f477df40048177aee08a1008ec06bf
David Olubaji <lildeyveed@gmail.com>
2024-07-12 17:22:08 +0100

    hide currency converter on aleo

--- app/(root)/unstake/_components/UnstakeAmountInputPad.tsx
+++ app/(root)/unstake/_components/UnstakeAmountInputPad.tsx
@@ -1,9 +1,13 @@
 "use client";
+import { getIsAleoNetwork } from "@/app/_services/aleo/utils";
 import { AmountInputPad } from "../../../_components/AmountInputPad";
 import { useUnstaking } from "../../../_contexts/UnstakingContext";
+import { useShell } from "@/app/_contexts/ShellContext";
 
 export const UnstakeAmountInputPad = () => {
+  const { network } = useShell();
   const { amountInputPad, stakedBalance, setStates, inputErrorMessage } = useUnstaking();
+  const isAleoNetwork = network && getIsAleoNetwork(network);
 
   return (
     <AmountInputPad
@@ -14,6 +18,7 @@ export const UnstakeAmountInputPad = () => {
         setStates({ coinAmountInput: val });
       }}
       error={inputErrorMessage}
+      hideCurrencyConversion={!!isAleoNetwork}
       {...amountInputPad}
     />
   );
