6b82fcff349ecacb0eb8e25f6c1dc8a3cabdb09b
David Olubaji <lildeyveed@gmail.com>
2024-07-12 17:10:54 +0100

    implement withdraw tx flow

--- app/(root)/unstake/_components/UnstakingProcedureDialog.tsx
+++ app/(root)/unstake/_components/UnstakingProcedureDialog.tsx
@@ -1,9 +1,26 @@
 "use client";
+import { useShell } from "@/app/_contexts/ShellContext";
 import { TxProcedureDialog } from "../../_components/TxProcedureDialog";
 import { useUnstaking } from "@/app/_contexts/UnstakingContext";
+import { useWallet } from "@/app/_contexts/WalletContext";
+import { useClaimingProcedures } from "@/app/_services/rewards/hooks";
+import { defaultNetwork } from "@/app/consts";
 
 export const UnstakingProcedureDialog = () => {
   const unstakingData = useUnstaking();
+  const { network: shellNetwork } = useShell();
+  const { activeWallet, address } = useWallet();
 
-  return <TxProcedureDialog data={unstakingData} type="unstake" dialog="unstakingProcedure" />;
+  const claimingData = useClaimingProcedures({
+    address: address,
+    network: shellNetwork || defaultNetwork,
+    wallet: activeWallet,
+  });
+
+  return (
+    <>
+      <TxProcedureDialog data={unstakingData} type="unstake" dialog="unstakingProcedure" />
+      <TxProcedureDialog data={claimingData} type="claim" dialog="claimingProcedure" />
+    </>
+  );
 };
