2eea346e6f1c8f62c5bc561061a75d47a4c8f2fa
vince19972 <vince19972@gmail.com>
2024-10-23 16:14:06 -0400

    chore: keep params on home link

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,17 +1,27 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { RootHeader } from "./RootHeader";
 import { useNetworkChange, useCurrencyChange } from "../../_contexts/ShellContext/hooks";
 import { useShell } from "@/app/_contexts/ShellContext";
+import { getLinkWithSearchParams } from "../../_utils/routes";
 
 export const Header = () => {
   const { network, stakingType } = useShell();
+  const searchParams = useSearchParams();
   const pathname = usePathname();
   const { activeNetwork, onUpdateRouter: onNetworkRouterUpdate } = useNetworkChange();
   const { activeCurrency, activeNetworkCurrency, onUpdateRouter: onCurrencyRouterUpdate } = useCurrencyChange();
 
   const isAleoLiquidUnstakePage = network === "aleo" && pathname === "/unstake" && stakingType === "liquid";
+  const homeLink = getLinkWithSearchParams(
+    {
+      network: searchParams.get("network") || undefined,
+      currency: searchParams.get("currency") || undefined,
+      userId: searchParams.get("userId") || undefined,
+    },
+    "",
+  );
 
   return (
     <RootHeader
@@ -25,6 +35,7 @@ export const Header = () => {
         activeNetwork,
         onNetworkChange: (net) => onNetworkRouterUpdate(net),
       }}
+      homeLink={homeLink}
     />
   );
 };
