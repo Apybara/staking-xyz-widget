b03b7aa84eda75c3e81e730d334543c5bc51a0c7
vince19972 <vince19972@gmail.com>
2024-10-31 11:28:06 -0400

    build: bump Cosmos related packages

--- app/_services/cosmos/cosmosKit/consts.ts
+++ app/_services/cosmos/cosmosKit/consts.ts
@@ -1,21 +1,17 @@
 import { assets, chains } from "chain-registry";
-import celestiatestnet3Chains from "../celestiatestnet3/chain.json";
-import celestiatestnet3AssetList from "../celestiatestnet3/assetlist.json";
-import cosmoshubtestnetChains from "../cosmoshubtestnet/chain.json";
-import cosmoshubtestnetAssetList from "../cosmoshubtestnet/assetlist.json";
 
 export const CosmosKitWalletVariants = ["keplr", "leap", "okx", "keplrMobile", "leapMobile"] as const;
 export const cosmosKitWalletVariants = [...CosmosKitWalletVariants];
 
 export const cosmosKitChains = [
   chains.filter((chain) => chain.chain_id === "celestia")[0] || "celestia",
-  celestiatestnet3Chains,
+  chains.filter((chain) => chain.chain_id === "mocha-4")[0] || "mocha-4",
   chains.filter((chain) => chain.chain_id === "cosmoshub-4")[0] || "cosmoshub-4",
-  cosmoshubtestnetChains,
+  chains.filter((chain) => chain.chain_id === "theta-testnet-001")[0] || "theta-testnet-001",
 ];
 export const cosmosKitAssets = [
   assets.filter((asset) => asset.chain_name === "celestia")[0],
-  celestiatestnet3AssetList,
+  assets.filter((asset) => asset.chain_name === "celestiatestnet3")[0],
   assets.filter((asset) => asset.chain_name === "cosmoshub")[0],
-  cosmoshubtestnetAssetList,
+  assets.filter((asset) => asset.chain_name === "cosmoshubtestnet")[0],
 ];
